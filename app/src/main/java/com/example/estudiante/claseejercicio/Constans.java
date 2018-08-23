a52583435a9de62eea63f60811d5e5eca2e9f4f3
Alejandro <Aleku999@gmail.com>
2018-08-23 12:50:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/estudiante/claseejercicio/Constans.java
@@ -0,0 +1,5 @@
+package com.example.estudiante.claseejercicio;
+
+public class Constans {
+    public static final int REQ_NEW_USER = 5000;
+}
---
+++ app/src/main/java/com/example/estudiante/claseejercicio/UserFormActivity.java
@@ -0,0 +1,29 @@
+package com.example.estudiante.claseejercicio;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class UserFormActivity extends AppCompatActivity {
+
+    private EditText edtUserFormName;
+    private EditText edtUserFormEmail;
+    private EditText edtUserFormUserName;
+
+    private Button btnUserFormFinish;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_form);
+
+        //Intent solicitud= getIntent();
+
+        edtUserFormName = findViewById(R.id.edt_userform_name);
+        edtUserFormEmail= findViewById(R.id.edt_userform_email);
+        edtUserFormUserName= findViewById(R.id.edt_userform_username);
+        btnUserFormFinish = findViewById(R.id.btn_title_userform_finish);
+    }
+}
